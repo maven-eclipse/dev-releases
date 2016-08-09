799c60a3ac3af4ac7d715840dcd24aba070acbd1
Leon Blakey <leon.m.blakey@gmail.com>
2016-08-09 07:49:55 -0400

    Added SWT 4.7M1, script to auto update

---
+++ newDevRelease.sh
@@ -0,0 +1,4 @@
+./scrape-check.sh
+bash -c "source scrape-swt.sh; set -e -x; stage_2_download"
+bash -c "source scrape-swt.sh; set -e -x; stage_3_extract"
+bash -c "source scrape-swt.sh; set -e -x; stage_4_install"
